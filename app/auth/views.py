97f7dc053d145b5bbed2686a5ed24587083e7154
msjy <msjy@163.com>
2017-10-01 17:44:38 +0800

    cancel the confirmed

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,7 +13,7 @@ from .forms import LoginForm, RegistrationForm, ChangePasswordForm,\
 def before_request():
     if current_user.is_authenticated:
         current_user.ping()
-        if not current_user.confirmed \
+        if current_user.confirmed \
                 and request.endpoint[:5] != 'auth.' \
                 and request.endpoint != 'static':
             return redirect(url_for('auth.unconfirmed'))
